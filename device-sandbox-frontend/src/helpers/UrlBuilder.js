9475612fb01b2362bc3f2f4fc10bbd6b6aa651e3
sam002696 <sadmansakib221@gmail.com>
2025-11-09 18:46:54 +0600

    added live server url to test frontend connections

--- device-sandbox-frontend/src/helpers/UrlBuilder.js
+++ device-sandbox-frontend/src/helpers/UrlBuilder.js
@@ -1,9 +1,9 @@
 class UrlBuilderHelper {
   deviceSimulatorApi() {
-    return `http://localhost:8000/api/v1`; // local connection
+    // return `http://localhost:8000/api/v1`; // local connection
     // return `http://192.168.1.243:8000/api`; // remote connection ip
     // return `http://localhost:8080/api`; // docker connection
-    // return `http://43.204.236.19:8080/api`; // live connection
+    return `https://api.conveysoft.dev/api/v1`; // live connection
   }
 }
 export const UrlBuilder = new UrlBuilderHelper();
