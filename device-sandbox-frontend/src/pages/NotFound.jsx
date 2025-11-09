3109eda7e2a6b6f751a28275b385f0eb4be9820c
sam002696 <sadmansakib221@gmail.com>
2025-11-09 20:06:24 +0600

    modified not found page design

--- device-sandbox-frontend/src/pages/NotFound.jsx
+++ device-sandbox-frontend/src/pages/NotFound.jsx
@@ -2,15 +2,26 @@ import { Link } from "react-router";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-full text-gray-400">
-      <h1 className="text-4xl mb-2 text-gray-200">404</h1>
-      <p className="mb-4">Page not found</p>
-      <Link
-        to="/"
-        className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-500"
-      >
-        Back Home
-      </Link>
-    </div>
+    <main className="flex flex-col items-center justify-center h-screen bg-[#03060c] px-6 py-24 sm:py-32 lg:px-8 ">
+      <div className="text-center">
+        <p className="text-base font-semibold text-indigo-600 dark:text-indigo-400">
+          404
+        </p>
+        <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-600 sm:text-7xl ">
+          Page not found
+        </h1>
+        <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 ">
+          Sorry, we couldn’t find the page you’re looking for.
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link
+            to="/"
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500"
+          >
+            Go back home
+          </Link>
+        </div>
+      </div>
+    </main>
   );
 }
