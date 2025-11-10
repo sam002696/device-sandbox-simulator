7c014e73c53e5beef9762a48972b937a3c6040ee
sam002696 <sadmansakib221@gmail.com>
2025-11-11 01:29:32 +0600

    changed the position of light visual

--- device-sandbox-frontend/src/components/light/LightCanvas.jsx
+++ device-sandbox-frontend/src/components/light/LightCanvas.jsx
@@ -5,7 +5,9 @@ const LightCanvas = () => {
   return (
     <div className="relative flex flex-col items-center justify-center w-full h-full">
       {/* Light visual representation */}
-      <LightVisual />
+      <div className="-translate-y-12">
+        <LightVisual />
+      </div>
       <div className="absolute bottom-10">
         {/* Light controls */}
         <LightControls />
